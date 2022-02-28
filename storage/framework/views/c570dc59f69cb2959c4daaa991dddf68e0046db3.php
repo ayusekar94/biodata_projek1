7b763a9d9ec4631520caa7a64a13dc2b20166359
ayusekar94 <92219304+ayusekar94@users.noreply.github.com>
2022-02-28 13:37:12 +0700

    Add files via upload

---
+++ storage/framework/views/c570dc59f69cb2959c4daaa991dddf68e0046db3.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="description" content="Biodata"/>
+        <meta name="Keywords" content="Biodata"/>
+        <meta charset="UTF-8"/>
+        <title>Biodata</title>
+        <link rel="stylesheet" href="<?php echo e(asset('css/style1.css')); ?>"/>
+        <!--<link rel="stylesheet" href="<"/>-->
+    </head>
+    <body>
+        <img src="<?php echo e(asset('img/2.png')); ?>" alt="">
+        <form action="#" style="width: 1000px"class="posisi";>
+        <h1>BIODATA</h1>
+        <fieldset class="h">
+        <table style="width: 980px;">
+            <tr>
+                <td rowspan="15" width="250px">
+                <img src="<?php echo e(asset('img/ananda.jpg')); ?>" width="250px" height="420px"/></td>
+            </tr>
+            <tr>
+                <td><b>NIM</b></td>
+                <td>:</td>
+                <td>2041720140</td>
+            </tr>
+            <tr>
+                <td><b>Nama Lengkap</b></td>
+                <td>:</td>
+                <td>Ananda Ayu Sekar Wiranti</td>
+            </tr>
+            <tr>
+                <td><b>Nama Panggilan</b></td>
+                <td>:</td>
+                <td>Sekar</td>
+            </tr>
+            <tr>
+                <td><b>Tempat, Tanggal Lahir</b></td>
+                <td>:</td>
+                <td>Malang, 18 Juni 2002</td>
+            </tr>
+            <tr>
+                <td><b>Umur</b></td>
+                <td>:</td>
+                <td>19 Tahun</td>
+            </tr>
+            <tr>
+                <td><b>Jenis Kelamin</b></td>
+                <td>:</td>
+                <td>Perempuan</td>
+            </tr>
+            <tr>
+                <td><b>Gol. Darah</b></td>
+                <td>:</td>
+                <td>A</td>
+            </tr>
+            <tr>
+                <td><b>Agama</b></td>
+                <td>:</td>
+                <td>Islam</td>
+            </tr>
+            <tr>
+                <td><b>Alamat</b></td>
+                <td>:</td>
+                <td>Jln.M.T Haryono 194-B, Malang</td>
+            </tr>
+            <tr>
+                <td><b>Pekerjaan</b></td>
+                <td>:</td>
+                <td>Mahasiswa</td>
+            </tr>
+            <tr>
+                <td><b>Kewarganegaraan</b></td>
+                <td>:</td>
+                <td>Indonesia</td>
+            </tr>
+            <tr>
+                <td><b>Riwayat</b></td>
+                <td>:</td>
+                <td colspan="1" alight="left">
+                <a href="<?php echo e(url('about')); ?>" style="text-decoration: none;">Riwayat</a>
+                </td>
+            </tr>
+        </table>
+        </fieldset>
+        </form>
+    </body>
+</html><?php /**PATH C:\xampp2\htdocs\cms\resources\views/home.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/ce12c0211b58ab1f7ad95d9b2edabeb60d862ebc.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="description" content="Biodata"/>
+        <meta name="Keywords" content="Biodata"/>
+        <meta charset="UTF-8"/>
+        <title>Biodata</title>
+        <link rel="stylesheet" href="<?php echo e(asset('css/style2.css')); ?>"/>
+    </head>
+    <body>
+        <form action="#" style="width: 1000px"class="posisi";>
+            <table style="width: 950px;">
+                <tr>
+                <td colspan="4" style="text-align: center; background-color: pink;color: purple"><b>Riwayat Jenjang Pendidikan</b></td>
+                </tr>
+                <tr>
+                    <td style="text-align: center">Jenjang Pendidikan</td>
+                    <td style="text-align: center">Keterangan</td>
+                    <td style="text-align: center">Bidang Jurusan</td>
+                    <td style="text-align: center">Tahun</td>
+                </tr>
+                <tr>
+                    <td>SEKOLAH DASAR</td>
+                    <td>SDN Merjosari 1, Malang </td>
+                    <td>Tidak ada</td>
+                    <td>2008 s.d 2013</td>
+                </tr>
+                <tr>
+                    <td>SEKOLAH MENENGAH PERTAMA</td>
+                    <td>SMP Laboratorium UM </td>
+                    <td>Tidak ada</td>
+                    <td>2013 s.d 2017</td>
+                </tr>
+                <tr>
+                    <td>SEKOLAH MENENGAH KEJURUAN</td>
+                    <td>SMKN 5 Malang</td>
+                    <td>Teknik Komputer dan Jaringan</td>
+                    <td>2017 s.d 2020 </td>
+                </tr>
+                <tr>
+                    <td>KULIAH</td>
+                    <td>POLITEKNIK NEGERI MALANG</td>
+                    <td>D4 Teknik Informatika</td>
+                    <td>2020 s.d Sekarang</td>
+                </tr>
+            </table>
+            <br>
+            <table style="width: 950px;">
+                <tr>
+                    <td colspan="4" style="text-align: center; background-color: pink;color: purple"><b>Informasi Umum</b></td>
+                </tr>
+                <tr>
+                    <td>Nomor Telephon</td>
+                    <td>:</td>
+                    <td>+62 853-3403-0518</td>
+                </tr>
+                <tr>
+                    <td>E-mail</td>
+                    <td>:</td>
+                    <td>ayu.wiranti181@gmail.com</td>
+                </tr>
+                <tr>
+                    <td>Hobi</td>
+                    <td>:</td>
+                    <td>Olahraga, Traveling, Nonton Drakor</td>
+                </tr>
+                    <td colspan="3" alight="right">
+                    <a href="home.blade.php" style="text-decoration: none;"></a><input type="button" onclick="history.back()" value="Kembali"/>
+                </td>
+            </table>
+        </form>
+        <img src="<?php echo e(asset('img/1.jpg')); ?>" alt="" class="center">
+    </body>
+</html><?php /**PATH C:\xampp2\htdocs\cms\resources\views/about.blade.php ENDPATH**/ ?>
